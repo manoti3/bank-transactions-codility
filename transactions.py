12fbafb1d5d9323cdf76eb81fa8df3fd71f4399e
manoti3 <hillarymanoti0@gmail.com>
2024-03-11 16:21:02 +0300

    completed assignment

---
+++ transactions.py
@@ -0,0 +1,24 @@
+# writing a function that that represents transaction amounts 
+def solution(A):
+    transactions=''
+
+
+
+
+    bank_transactions = [100,100,100,-10]
+    transactions = solution(bank_transactions)
+    print(transactions)
+
+    
+
+
+
+# a funcion that returns transaction dates and returns the final balance of the account at the end of the year
+def solutioon(D):
+    transactions_dates=''
+
+
+
+    bank_transactions_dates = ["2020-12-31", "2020-12-22", "2020-12-29"]
+    dates = solution(bank_transactions_dates)
+    print(dates)
